3b020c0b1121c3486dccb1a496736bf7506417a0
hanks7 <474664736@qq.com>
2016-06-01 11:48:53 +0800

    全部提交

---
+++ app/src/main/java/com/hanks7/houjianjuntest/welcome/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.hanks7.houjianjuntest.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hanks7.houjianjuntest.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
