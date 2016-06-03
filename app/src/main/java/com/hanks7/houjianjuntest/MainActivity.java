da91ee8e205c8fdfcaf01cd993fa0b5c1ecfc54b
unknown <>
2016-06-03 14:58:54 +0800

    实验 json解析 Butterknife插件测试.....

--- app/src/main/java/com/hanks7/houjianjuntest/MainActivity.java
+++ app/src/main/java/com/hanks7/houjianjuntest/MainActivity.java
@@ -5,34 +5,77 @@ import android.view.View;
 import android.widget.Button;
 
 import com.hanks7.houjianjuntest.base.BaseActivity;
+import com.hanks7.houjianjuntest.util.UtilGson;
 import com.hanks7.houjianjuntest.util.UtilToast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
 
 public class MainActivity extends BaseActivity {
 
     @Bind(R.id.button)
     Button button;
 
-
-
+    private String s;
+    private TestBean sd;
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        isHome = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UtilToast.showToast("fasdfasdf");
+        hide();
         ButterKnife.bind(this);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intentLeftToRight(SecondActivity.class);
+                intentLeftToRight(SecondActivity.class, bundle);
             }
         });
-        Button button2=new Button(this);
 
+        parseJson();
+        bundle=new Bundle();
+        bundle.putParcelable("sd", sd);
+
+
+
+    }
+
+    private void hide() {
+        s="{ \"programmers\": [\n" +
+                "\n" +
+                "        { \"firstName\": \"Brett\", \"lastName\":\"McLaughlin\", \"email\": \"aaaa\" },\n" +
+                "\n" +
+                "        { \"firstName\": \"Jason\", \"lastName\":\"Hunter\", \"email\": \"bbbb\" },\n" +
+                "\n" +
+                "        { \"firstName\": \"Elliotte\", \"lastName\":\"Harold\", \"email\": \"cccc\" }\n" +
+                "\n" +
+                "        ],\n" +
+                "\n" +
+                "        \"authors\": [\n" +
+                "\n" +
+                "        { \"firstName\": \"Isaac\", \"lastName\": \"Asimov\", \"genre\": \"science fiction\" },\n" +
+                "\n" +
+                "        { \"firstName\": \"Tad\", \"lastName\": \"Williams\", \"genre\": \"fantasy\" },\n" +
+                "\n" +
+                "        { \"firstName\": \"Frank\", \"lastName\": \"Peretti\", \"genre\": \"christian fiction\" }\n" +
+                "\n" +
+                "        ],\n" +
+                "\n" +
+                "        \"musicians\": [\n" +
+                "\n" +
+                "        { \"firstName\": \"Eric\", \"lastName\": \"Clapton\", \"instrument\": \"guitar\" },\n" +
+                "\n" +
+                "        { \"firstName\": \"Sergei\", \"lastName\": \"Rachmaninoff\", \"instrument\": \"piano\" }\n" +
+                "\n" +
+                "        ] }";
+    }
+
+    public void parseJson(){
+        sd = (TestBean) UtilGson.fromJson(s, TestBean.class);
+        UtilToast.showToast(sd.getProgrammers().get(0).getEmail());
 
     }
 }
--- app/src/main/java/com/hanks7/houjianjuntest/SecondActivity.java
+++ app/src/main/java/com/hanks7/houjianjuntest/SecondActivity.java
@@ -1,10 +1,12 @@
 package com.hanks7.houjianjuntest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.hanks7.houjianjuntest.base.BaseActivity;
+import com.hanks7.houjianjuntest.util.UtilToast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,6 +20,11 @@ public class SecondActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ButterKnife.bind(this);
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        UtilToast.showToast(bundle.getParcelable("sd").toString());
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
